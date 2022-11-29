49dcfd803aa1679df2ac61ca3056ac5e0095c5b9
Buteo <oliv.collet@yahoo.fr>
2022-11-29 10:45:58 +0100

    mend

--- src/visitor.console/Visitors/Contracts/DescriptionVisitor.cs
+++ src/visitor.console/Visitors/Contracts/DescriptionVisitor.cs
@@ -9,19 +9,8 @@ public class DescriptionVisitor : IContractVisitor
     public string Description { get; set; } = null!;
     public string PrintableDescription { get; set; } = null!;
     public ReferenceVisitor ReferenceVisitor { get; set; } = new ReferenceVisitor();
-    // public string LastVisitedTypeName { get; set; } = null!;
 
-    // public override string? ToString()
-    // {
-    //     return LastVisitedTypeName switch
-    //     {
-    //         nameof(Contractor) => $"{Description.Length}",
-    //         nameof(Consulting) => GetDescriptionHash(),
-    //         _ => "Undefined contract type",
-    //     };
-    // }
-
-    private string GetDescriptionHash()
+     private string GetDescriptionHash()
     {
         var descriptionInBytes = Encoding.UTF8.GetBytes(Description);
         using var encoder = SHA256.Create();
@@ -37,18 +26,13 @@ public class DescriptionVisitor : IContractVisitor
 
     public void Visit(Contractor element)
     {
-        // LastVisitedTypeName = nameof(Contractor);
         element.Accept(ReferenceVisitor);
-        //ReferenceVisitor.Visit(element!);
         Description = $"{ReferenceVisitor.Reference}/{element?.Resource?.Name}";
         PrintableDescription = Description.Length.ToString();
     }
 
     public void Visit(Consulting element)
     {
-        // Consulting._id + liste des couples name/id des Resource, exemple avec 2 Resource : C21345-3[Pierre-14, Jean-2, Paul-3]
-        // LastVisitedTypeName = nameof(Consulting);
-        // ReferenceVisitor.Visit(element!);
         element.Accept(ReferenceVisitor);
         var displayNameVisitor = new Resources.DisplayNameVisitor();
         var segments = new List<string>();
